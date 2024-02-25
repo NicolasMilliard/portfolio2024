638fa160928ad4e1b8ab55f194b575f838391486
Nicolas Milliard <contact@nicolasmilliard.fr>
2024-02-25 17:03:42 +0100

    Add logo

--- app/components/Navbar/Logo/index.tsx
+++ app/components/Navbar/Logo/index.tsx
@@ -1,5 +1,19 @@
+// Components
+import { Title } from "@components/Title";
+import Image from "next/image";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div className="flex items-center">
+      <Image
+        src="/images/logo.png"
+        alt="Nicolas Milliard"
+        width={190}
+        height={190}
+      />
+      <Title>Nicolas Milliard</Title>
+    </div>
+  );
 };
 
 export default Logo;
