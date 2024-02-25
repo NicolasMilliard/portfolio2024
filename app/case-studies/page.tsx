b2a56325bba36b37f1cea019518225894ee32537
Nicolas Milliard <contact@nicolasmilliard.fr>
2024-02-25 16:47:54 +0100

    Add new pages

---
+++ app/case-studies/page.tsx
@@ -0,0 +1,13 @@
+// Types
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Case studies | Nicolas Milliard",
+  description: "My latest case studies.",
+};
+
+const CaseStudies = () => {
+  return <div>CaseStudies</div>;
+};
+
+export default CaseStudies;
