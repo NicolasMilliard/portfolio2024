b29c6567c79ff212ef01dee963b16037f853f8af
Nicolas Milliard <contact@nicolasmilliard.fr>
2024-02-27 23:11:00 +0100

    Update Navbar styling based on scroll position

--- app/containers/Navbar/index.tsx
+++ app/containers/Navbar/index.tsx
@@ -7,7 +7,8 @@ import Menu from "@components/Navbar/Menu";
 
 const Navbar = () => {
   const [scrollPosition, setScrollPosition] = useState<number>(0);
-  const [dynamicBorder, setDynamicBorder] = useState<string>("");
+  const [dynamicBorder, setDynamicBorder] = useState<string>("rounded-3xl");
+  const [dynamicHeight, setDynamicHeight] = useState<string>("h-128");
 
   const handleScroll = () => {
     setScrollPosition(window.scrollY);
@@ -16,9 +17,13 @@ const Navbar = () => {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
-    scrollPosition > 40
-      ? setDynamicBorder("rounded-b-3xl")
-      : setDynamicBorder("rounded-3xl");
+    if (scrollPosition > 40) {
+      setDynamicBorder("rounded-b-3xl");
+      setDynamicHeight("h-96");
+    } else {
+      setDynamicBorder("rounded-3xl");
+      setDynamicHeight("h-128");
+    }
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -26,14 +31,16 @@ const Navbar = () => {
   }, [scrollPosition]);
 
   return (
-    <nav className="sticky top-0 h-128 w-full">
+    <nav className="sticky top-0">
       <div
-        className={`absolute z-20 flex h-128 w-full items-center justify-between bg-white px-60 py-40 duration-100 ${dynamicBorder}`}
+        className={`absolute z-20 flex ${dynamicHeight} w-full items-center justify-between bg-white px-60 py-40 duration-100 ${dynamicBorder}`}
       >
         <Logo />
         <Menu />
       </div>
-      <div className="absolute left-0 top-0 z-10 h-128 w-full  rounded-3xl bg-gradient-to-r from-ultramarine to-congo_pink px-60 py-40 blur-lg"></div>
+      <div
+        className={`absolute left-0 top-0 z-10 ${dynamicHeight} w-full  rounded-3xl bg-gradient-to-r from-ultramarine to-congo_pink px-60 py-40 blur-lg duration-100`}
+      ></div>
     </nav>
   );
 };
