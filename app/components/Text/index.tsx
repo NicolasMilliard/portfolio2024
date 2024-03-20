61dc07af89ab87601b82bf0da9ffe5cf907af83c
Nicolas Milliard <contact@nicolasmilliard.fr>
2024-03-20 22:57:55 +0100

    Update Kopo page

--- app/components/Text/index.tsx
+++ app/components/Text/index.tsx
@@ -1,13 +1,29 @@
 // Types
 import type { ReactNode } from "react";
 
-export const Paragraph = ({ children }: { children: ReactNode }) => {
-  return <p className="max-w-200 font-book text-black">{children}</p>;
+export const Paragraph = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <p className={`${className} max-w-200 font-book text-black`}>{children}</p>
+  );
 };
 
-export const GradientParagraph = ({ children }: { children: ReactNode }) => {
+export const GradientParagraph = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
   return (
-    <p className="w-fit bg-gradient-to-r from-ultramarine to-congo_pink bg-clip-text font-normal text-transparent">
+    <p
+      className={`${className} w-fit bg-gradient-to-r from-ultramarine to-congo_pink bg-clip-text font-normal text-transparent`}
+    >
       {children}
     </p>
   );
