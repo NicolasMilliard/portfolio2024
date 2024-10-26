344008af39856fb911ed0f221e8d26c5f3877562
Nicolas Milliard <contact@nicolasmilliard.fr>
2024-10-26 12:13:45 +0200

    Remove random images

--- app/containers/Home/Introduction/index.tsx
+++ app/containers/Home/Introduction/index.tsx
@@ -2,18 +2,13 @@
 import { Paragraph } from '@components/Text';
 import { MainTitle, Title } from '@components/Title';
 import Image from 'next/image';
-// Types
-import { CaseStudiesImages } from '../CaseStudies/CaseStudyImage';
 
 const Introduction = () => {
-  const images = [...Object.keys(CaseStudiesImages), 'prism'];
-  const randomImage = images[Math.floor(Math.random() * images.length)];
-
   return (
     <section className="mb-32 flex items-center justify-center gap-20 lg:px-20 xl:px-40">
       <div className="hidden lg:block">
         <Image
-          src={`/images/assets/${randomImage}.png`}
+          src={`/images/assets/prism.png`}
           alt="Nicolas Milliard"
           width={360}
           height={335}
